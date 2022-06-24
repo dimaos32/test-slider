4177571be2d7e764c2b9fa66b6a8b31ac2c6f71a
Dmitrii Osadchenko <dimaos@protonmail.com>
2022-06-24 19:31:35 +0300

    Добавляет пагинацию

--- source/js/modules/init-slider-hero.js
+++ source/js/modules/init-slider-hero.js
@@ -1,4 +1,5 @@
 const breakpoint767 = window.matchMedia('(max-width: 767px');
+const breakpoint1023 = window.matchMedia('(max-width: 1023px');
 
 const initSliderhero = () => {
   const sliders = document.querySelectorAll('.slider-hero');
@@ -9,8 +10,38 @@ const initSliderhero = () => {
 
   sliders.forEach((slider) => {
     const container = slider.querySelector('.slider-hero__container');
+    const slides = slider.querySelectorAll('.slider-hero__slide');
+    const paginationElement = slider.querySelector('.slider-hero__pagination');
     const prevBtn = slider.querySelector('.slider-hero__btn--prev');
     const nextBtn = slider.querySelector('.slider-hero__btn--next');
+
+    const mobilePagination = {
+      el: paginationElement,
+      clickable: true,
+      bulletClass: 'slider-hero__bullet',
+      bulletActiveClass: 'is-active',
+      renderBullet: (index, className) => `<div class="${className}"></div>`,
+    };
+
+    const desktopPagination = {
+      el: paginationElement,
+      clickable: true,
+      bulletClass: 'slider-hero__bullet',
+      bulletActiveClass: 'is-active',
+      renderBullet: (index, className) => {
+        const slide = slides[index];
+        const title = slide.querySelector('.slider-hero__title');
+        const desc = slide.querySelector('.slider-hero__desc .not-mobile');
+
+        return `<div class="${className}">
+          <img class="slider-hero__bullet-img" src=${slide.dataset.paginationImg}>
+          <div class="slider-hero__bullet-content">
+            <p class="slider-hero__bullet-desc">${desc.textContent.split(' по')[0]}</p>
+            <p class="slider-hero__bullet-title">${title.textContent}</p>
+          </div>
+        </div>`;
+      },
+    };
 
     let swiper;
 
@@ -22,16 +53,17 @@ const initSliderhero = () => {
         mousewheel: {
           forceToAxis: true,
         },
+        pagination: mobilePagination,
         navigation: {
           prevEl: prevBtn,
           nextEl: nextBtn,
           disabledClass: 'slider-hero__btn--disabled',
         },
-        // breakpoints: {
-        //   768: {
-        //     spaceBetween: 30,
-        //   },
-        // },
+        breakpoints: {
+          1024: {
+            pagination: desktopPagination,
+          },
+        },
       });
     };
 
@@ -43,6 +75,7 @@ const initSliderhero = () => {
     initSwiper();
 
     breakpoint767.addListener(breakpointChecker);
+    breakpoint1023.addListener(breakpointChecker);
   });
 };
 
